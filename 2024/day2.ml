84657d3c298a60818e6d1095f33cf349be9cec94
etudelagirau <emmanuel.tudela-girau@etu.u-bordeaux.fr>
2024-12-03 15:14:44 +0100

    J'en ai marre

--- 2024/day2.ml
+++ 2024/day2.ml
@@ -7,7 +7,7 @@ let lines_to_intlist lines =
 
 let list_diff list =
     let rec aux acc = function
-        | [] | [ _ ] -> acc
+        | [] | [ _ ] -> List.rev acc
         | x :: ((y :: t) as xs) -> aux ((y - x) :: acc) xs
     in
     aux [] list;;
@@ -15,21 +15,26 @@ let list_diff list =
 let count elm l = List.length @@ List.filter (fun x -> x = elm) l;;
 
 let is_same_sign x y = x * y > 0;;
-let rec list_is_same_sign = function
-    | [] | [ _ ] -> true
-    | x :: ((y :: t) as xs) ->
-        if is_same_sign x y then
-            list_is_same_sign xs
-        else
-            false;;
 
-let list_stability list =
+let list_is_stable list =
     let diff = list_diff list in
-    list_is_same_sign diff && not (List.mem 0 diff);;
+    let rec aux = function
+    | [] -> true
+    | [ x ] -> x <> 0 && (abs x) <= 3
+    | x :: ((y :: t) as xs) ->
+        (is_same_sign x y && (abs x) <= 3 && x <> 0) && aux xs
+    in
+    aux diff;;
 
-let input_text = read_lines "inputs/test.txt";;
+let list_stability_list list =
+    let diff = list_diff list in
+    let aux sign prev acc = function
+        | [] -> true
+        | [x] -> 
+
+let input_text = read_lines "inputs/day2.txt";;
 let input_data = lines_to_intlist input_text;;
 
 let () =
-    let results = List.map list_stability input_data in
+    let results = List.map list_is_stable input_data in
     Printf.printf "%d\n" (count true results);;
